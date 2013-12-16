dbabcb39fb1c582c2fe7120a99cf8eb8d9a30298
Frank Harris <fcharris@gmail.com>
2013-12-16 08:44:49 -0500

    Updated index template; added search

--- pelicanconf.py
+++ pelicanconf.py
@@ -45,17 +45,9 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-'''
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-'''
+TWITTER = 'http://www.twitter.com/hirefrank'
+LINKEDIN = 'http://www.linkedin.com/in/hirefrank'
+MAIL = 'frank@hirefrank.com'
 
 DEFAULT_PAGINATION = 10
 
