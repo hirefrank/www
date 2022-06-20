4238d976183b28dd073e5f57b4f5136e4951aad3
Frank Harris <frank@hirefrank.com>
2022-06-19 20:51:32 -0400

    redirect support?

--- server.ts
+++ server.ts
@@ -1,9 +1,24 @@
 import Server from "https:/deno.land/x/lume/core/server.ts";
+import redirects from "https:/deno.land/x/lume/middlewares/redirects.ts";
+
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
+
+server.use(redirects({
+  redirects: {
+    "/from/": "/to/",
+    "/from2/": "/to2/",
+
+    // Use an object to configure the status code. (301 by default)
+    "/from3/": {
+      to: "/to2/",
+      code: 302,
+    },
+  },
+}));
 
 server.start();
 
