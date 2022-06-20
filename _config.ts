9cc511763c8eebf970af8fdac5d16bfe19ac83ed
Frank Harris <frank@hirefrank.com>
2022-06-19 20:38:21 -0400

    added github action for dd

--- _config.ts
+++ _config.ts
@@ -1,12 +1,17 @@
 import lume from "lume/mod.ts";
-import date from "lume/plugins/date.ts";
-import metas from "lume/plugins/metas.ts";
-import sass from "lume/plugins/sass.ts";
+//import date from "lume/plugins/date.ts";
+//import metas from "lume/plugins/metas.ts";
+//import sass from "lume/plugins/sass.ts";
 
-const site = lume();
+const site = lume({
+  src: "./src",
+});
 
-site.use(date());
-site.use(metas());
-site.use(sass());
+//site.use(date());
+//site.use(metas());
+//site.use(sass());
+
+site.copy("public", ".");
+site.copy("assets", ".");
 
 export default site;
