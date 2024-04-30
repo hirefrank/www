f505ce05fcc8d3976f86324d12a9057ddaffc166
Frank Harris <fcharris@gmail.com>
2024-04-30 16:00:28 -0400

    Turn off litestream

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,14 @@ set -e
 DB_PATH=/var/lib/ghost/data/ghost.db
 
 # Restore the database if it does not already exist.
-if [ -f $DB_PATH ]; then
-	echo "Database already exists, skipping restore"
-else
-	echo "No database found, restoring from replica if exists"
-	# litestream restore -if-replica-exists $DB_PATH
-    sleep 30s # try to give it time to sync db
-fi
+# if [ -f $DB_PATH ]; then
+# 	echo "Database already exists, skipping restore"
+# else
+# 	echo "No database found, restoring from replica if exists"
+# 	litestream restore -if-replica-exists $DB_PATH
+#     sleep 30s # try to give it time to sync db
+# fi
 
 # Run litestream with your app as the subprocess.
-exec litestream replicate -exec "node current/index.js"
+# exec litestream replicate -exec "node current/index.js"
+node current/index.js
