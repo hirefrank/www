1c785222aba9b6d3e7644a1e6b978da108d9ea36
arturkotanidi12 <arturkotanidi156@gmail.com>
2024-05-17 09:50:35 +0400

    fixies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,70 +1,72 @@
-function themeIcons() {
+document.addEventListener('DOMContentLoaded', function () {
+  function themeIcons() {
+    const storedTheme = localStorage.getItem("theme");
+
+    if (storedTheme === 'dark') {
+      document.querySelector(".light-icon").classList.add("hidden");
+      document.querySelector(".dark-icon").classList.remove("hidden");
+    } else {
+      document.querySelector(".dark-icon").classList.add("hidden");
+      document.querySelector(".light-icon").classList.remove("hidden");
+    }
+  }
+
+  const toggleTheme = () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    themeIcons()
+  };
+
+  document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
+
   const storedTheme = localStorage.getItem("theme");
 
-  if (storedTheme === 'dark') {
-    document.querySelector(".light-icon").classList.add("hidden");
-    document.querySelector(".dark-icon").classList.remove("hidden");
+  if (storedTheme) {
+    document.documentElement.setAttribute("data-theme", storedTheme);
   } else {
-    document.querySelector(".dark-icon").classList.add("hidden");
-    document.querySelector(".light-icon").classList.remove("hidden");
+    document.documentElement.setAttribute("data-theme", "light"); // or any default theme
   }
-}
-
-const toggleTheme = () => {
-  const currentTheme = document.documentElement.getAttribute("data-theme");
-  const newTheme = currentTheme === "dark" ? "light" : "dark";
-
-  document.documentElement.setAttribute("data-theme", newTheme);
-  localStorage.setItem("theme", newTheme);
 
   themeIcons()
-};
 
-document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
+  document.querySelector(".menu-button")?.addEventListener("click", () => {
+    const element = document.querySelector('.mobile-menu');
 
-const storedTheme = localStorage.getItem("theme");
+    if (element) {
+      element.classList.remove("hidden");
+    }
+  });
 
-if (storedTheme) {
-  document.documentElement.setAttribute("data-theme", storedTheme);
-} else {
-  document.documentElement.setAttribute("data-theme", "light"); // or any default theme
-}
+  document.querySelector(".menu-close-button")?.addEventListener("click", () => {
+    const element = document.querySelector('.mobile-menu');
 
-themeIcons()
+    if (element) {
+      element.classList.add("hidden");
+    }
+  });
 
-document.querySelector(".menu-button")?.addEventListener("click", ()  => {
-  const element = document.querySelector('.mobile-menu');
-
-  if (element) {
-    element.classList.remove("hidden");
-  }
-});
-
-document.querySelector(".menu-close-button")?.addEventListener("click", ()  => {
-  const element = document.querySelector('.mobile-menu');
-
-  if (element) {
-    element.classList.add("hidden");
-  }
-});
-
-document.querySelector(".article-back")?.addEventListener("click", ()  => {
-  history.back()
-});
+  document.querySelector(".article-back")?.addEventListener("click", () => {
+    history.back()
+  });
 
 
 // header navbar active item
-function activeNav() {
-  const pathName = window.location.pathname
+  function activeNav() {
+    const pathName = window.location.pathname
 
-  if (pathName) {
-    const el = document.getElementById(pathName);
+    if (pathName) {
+      const el = document.getElementById(pathName);
 
-    el.classList.add('active-nav')
+      el.classList.add('active-nav')
 
-    const spanElement = el.querySelector('span');
-    spanElement.classList.remove('hidden')
+      const spanElement = el.querySelector('span');
+      spanElement.classList.remove('hidden')
+    }
   }
-}
 
-activeNav()
+  activeNav()
+})
