8ba8f1fc60a23704cc47fa3b8754ff09b105b082
arturkotanidi12 <arturkotanidi156@gmail.com>
2024-05-18 14:17:03 +0400

    fixies 2

--- src/_components/layout/navbar.tsx
+++ src/_components/layout/navbar.tsx
@@ -82,7 +82,7 @@ export default (
                                 className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark-bg-zinc-800/90 dark-ring-white/10 dark-hover:ring-white/20">
                           <svg viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
                                aria-hidden="true"
-                               className="light-icon h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark-hidden dark-fill-teal-50 dark-stroke-teal-500 dark-group-hover:fill-teal-50 dark-group-hover:stroke-teal-600">
+                               className="light-icon h-6 w-6 fill-zinc-100 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark-hidden dark-fill-teal-50 dark-group-hover:fill-teal-50 dark-group-hover:stroke-teal-600 stroke-teal-500">
                             <path
                               d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"></path>
                             <path
@@ -91,7 +91,7 @@ export default (
                           </svg>
 
                           <svg viewBox="0 0 24 24" aria-hidden="true"
-                               className="dark-icon hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark-block dark-group-hover:stroke-zinc-400 dark-stroke-teal-500">
+                               className="dark-icon hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark-block dark-group-hover:stroke-zinc-400">
                             <path
                               d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
                               stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
