9851a3bb09bf8174fbbc21b02b582249fb590a11
Frank Harris <fcharris@gmail.com>
2024-06-21 10:51:49 -0400

    update

--- src/_components/layout/navbar.tsx
+++ src/_components/layout/navbar.tsx
@@ -56,7 +56,7 @@ export default (
                       <nav className="pointer-events-auto hidden md:block">
                         <ul
                           className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark-bg-zinc-800/90 dark-text-zinc-200 dark-ring-white/10">
-                          {items.map(({data}: { data }) => (
+                          {items.map((data) => (
                             <li itemProp="url" className="navbar-li" id={data.url}>
                                <comp.layout.link
                                 className="relative block px-3 py-2 transition hover:text-teal-500 dark-hover:text-teal-400"
