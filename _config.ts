91e34741e59a173ce08c4176614a430f9ec0b3cd
Frank Harris <fcharris@gmail.com>
2024-06-21 15:47:16 -0400

    cleaned up config

--- _config.ts
+++ _config.ts
@@ -2,13 +2,11 @@ import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import slugify_urls from "lume/plugins/slugify_urls.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
-import sass from "lume/plugins/sass.ts";
 import terser from "lume/plugins/terser.ts";
 import inline from "lume/plugins/inline.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
-import mdx from "lume/plugins/mdx.ts";
 import unified from "lume/plugins/remark.ts";
 
 import remarkPlugins from "#plugins/unified/remark/mod.ts";
@@ -39,12 +37,10 @@ site
     },
   }))
   .use(jsx())
-  .use(sass())
   .use(unified({
     remarkPlugins,
     rehypePlugins,
   }))
-  .loadAssets([".js"])
   .use(terser())
   .use(inline())
   .use(tailwindcss({
@@ -54,6 +50,5 @@ site
   .use(sitemap({
     query: "indexable=true",
   }))
-  .use(mdx(/* Options */));
 
 export default site;
